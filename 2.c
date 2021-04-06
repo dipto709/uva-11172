e789880994d4cabb12fe99e47430865925b23200
dipto709 <82051242+dipto709@users.noreply.github.com>
2021-04-06 23:56:06 +0600

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b,i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+    scanf("%d%d",&a,&b);
+    printf("%d\n",(a/3)*(b/3));
+        }
+    return 0;
+    }
+    
